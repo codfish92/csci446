81f2c767f2f55d0ce482b927497c2a7b086f6e55
Drew Koelling <akoellin@mymail.mines.edu>
2014-02-04 23:09:59 -0700

    finished fixtures

--- project04/pets/app/controllers/pets_controller.rb
+++ project04/pets/app/controllers/pets_controller.rb
@@ -71,6 +71,6 @@ class PetsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pet_params
-      params.require(:pet).permit(:name, :age, :primaryType, :secondaryType, :image)
+      params.require(:pet).permit(:name, :age, :primaryType, :secondaryType, :image, :sort)
     end
 end
